fad2519d78aa0f94af2bda8f327704c25c028131
Andrea Parodi <andrea@parro.it>
2016-02-08 22:37:04 +0100

    adde second window to tests

--- test.js
+++ test.js
@@ -6,9 +6,18 @@ electron.app.on('ready', () => {
     show: true
   });
 
+  const win2 = new electron.BrowserWindow({
+    show: true
+  });
+
   win.webContents.executeJavaScript(`
     require('${__dirname}').install();
   `);
 
+  win2.webContents.executeJavaScript(`
+    require('${__dirname}').install();
+  `);
+
   win.loadURL('https://google.com');
+  win2.loadURL('https://google.com');
 });
